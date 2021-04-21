78842d000d1f71cdcd2e13ce1da088a6f431f368
Pavlova Marina <marizza.mil89@gmail.com>
2021-04-21 19:34:12 +0300

    Add project files.

---
+++ ValueConverter/ValueConverterOne.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ValueConverter
+{
+    class ValueConverterOne : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if (value.ToString().Length < 3)
+            {
+               return "Short";
+            }
+            else if (value.ToString().Length < 5)
+            {
+                return "Medium";
+            }
+            else {
+                return "Long";
+            }
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
